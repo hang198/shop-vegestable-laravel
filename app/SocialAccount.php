cddb790ba312723c3d8e5d05bfd4a10c9b50dc24
hang198 <hang17111998@gmail.com>
2019-10-17 13:54:40 +0700

    fix login fb

--- app/SocialAccount.php
+++ app/SocialAccount.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SocialAccount extends Model
 {
     protected $fillable = ['user_id', 'provider_user_id', 'provider'];
-    protected $primaryKey = 'user-id';
+    protected $primaryKey = 'user_id';
 
     public function user()
     {
